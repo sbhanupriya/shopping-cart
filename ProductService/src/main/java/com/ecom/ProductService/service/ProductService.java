1249d13c3a1ae634e50e9d0f0b3e76ded04285f5
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-06-16 02:55:57 +0530

    Product Service - Controller/Repository/Model/Service/Entity Boilerplate code @Service, @Reository, @RestController, @RequestMapping, @Entity, @Data, @Builder, @AllArgsConstructor, @NoArgsConstructor application.properties file changed to application.yaml config

---
+++ ProductService/src/main/java/com/ecom/ProductService/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.ecom.ProductService.service;
+
+public interface ProductService {
+}
---
+++ ProductService/src/main/java/com/ecom/ProductService/service/ProductSeviceImpl.java
@@ -0,0 +1,7 @@
+package com.ecom.ProductService.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductSeviceImpl implements ProductService{
+}
