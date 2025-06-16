a3affcf679c99359f873d561730faa48c6374863
sbhanupriya017 <sbhanupriya017@gmail.com>
2025-06-16 09:17:46 +0530

    add addproduct & getproduct api

--- ProductService/src/main/java/com/ecom/ProductService/service/ProductService.java
+++ ProductService/src/main/java/com/ecom/ProductService/service/ProductService.java
@@ -1,4 +1,10 @@
 package com.ecom.ProductService.service;
 
+import com.ecom.ProductService.model.ProductRequest;
+import com.ecom.ProductService.model.ProductResponse;
+
 public interface ProductService {
+    long addProduct(ProductRequest productRequest);
+
+    ProductResponse getProductById(Long productId);
 }
--- ProductService/src/main/java/com/ecom/ProductService/service/ProductSeviceImpl.java
+++ ProductService/src/main/java/com/ecom/ProductService/service/ProductSeviceImpl.java
@@ -1,7 +1,41 @@
 package com.ecom.ProductService.service;
 
+import com.ecom.ProductService.entity.Product;
+import com.ecom.ProductService.model.ProductRequest;
+import com.ecom.ProductService.model.ProductResponse;
+import com.ecom.ProductService.repository.ProductRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.BeanUtils.*;
+
 @Service
+@Log4j2
 public class ProductSeviceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public long addProduct(ProductRequest productRequest) {
+        log.info("Adding product...");
+        Product product = Product.builder()
+                .productName(productRequest.getName())
+                .price(productRequest.getPrice())
+                .quantity(productRequest.getQuantity())
+                .build();
+        productRepository.save(product);
+        log.info("Product Added...");
+        return product.getProductId();
+    }
+
+    @Override
+    public ProductResponse getProductById(Long productId) {
+        log.info("Get the product for Id: {}", productId);
+        Product product = productRepository.findById(productId).orElseThrow(() -> new RuntimeException("Product with given id not found"));
+        ProductResponse productResponse = new ProductResponse();
+        copyProperties(product, productResponse);
+        return productResponse;
+    }
 }
